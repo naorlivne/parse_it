b9df415a23736fd949496bb3c1b13136d22c2ec9
Naor Livne <naor@mintigo.com>
2019-04-23 15:20:49 +0300

    adding type estimate function

--- parse_this/command_line_args/command_line_args.py
+++ parse_this/command_line_args/command_line_args.py
@@ -1,3 +1,1 @@
 # TODO - read command line args
-
-# TODO - have option to guesstimate the type (so not all be string) that's false by default
--- parse_this/envvars/envvars.py
+++ parse_this/envvars/envvars.py
@@ -2,6 +2,4 @@
 
 # TODO - have option to auto uppercase envvars that's true by default
 
-# TODO - have option to guesstimate the type (so not all be string) that's false by default
-
 # TODO - have option to have prefix for all envvars
