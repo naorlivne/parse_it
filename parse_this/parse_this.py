66e52139d22b555630608cdfe7a2621c8f6c2457
Naor Livne <naor@mintigo.com>
2019-04-23 15:59:34 +0300

    adding command line args support

--- parse_this/parse_this.py
+++ parse_this/parse_this.py
@@ -5,7 +5,7 @@ class parse_this:
         # TODO - have the order list be editable
         # TODO - have a editable global_default_value of None
         # TODO - have trigger to auto upper case envvars that's true by default
-        # TODO - have a trigger to guesstimate the envvars, ini files & cli args types
+        # TODO - have a trigger to guesstimate the envvars, ini files & cli args types that's true by default
         # TODO - have a location to look for config files be definable by the user with default location to current folder
         # TODO - narrow down the possible list to only files that exist in the desired location
         # TODO - have the user ability to have only the highest priority type take affect for all config variables rather then on a per variable basis
