b54af9c21ba59f395e2616843a00117709002cb9
Naor Livne <naor@mintigo.com>
2019-05-06 12:20:00 +0300

    starting to use github releases do create new releases

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 __author__ = 'Naor Livne'
 __author_email__ = 'naorlivne@gmail.com'
-__version__ = os.environ("TRAVIS_TAG")
+__version__ = os.environ["TRAVIS_TAG"]
 
 # read the README.md file for the long description of the package
 with open('README.md') as f:
