dae0f337c19256aeec1fefe90051526cbac7dd3c
Naor Livne <naor@mintigo.com>
2019-07-16 15:33:38 +0300

    removing unneeded line for a unit test

--- test/test_prase_it.py
+++ test/test_prase_it.py
@@ -860,8 +860,7 @@ class BaseTests(TestCase):
     def test_parser_read_configuration_variable_check_allowed_not_in_types_list(self):
         with self.assertRaises(TypeError):
             parser = ParseIt(config_folder_location=test_files_location)
-            reply_json = parser.read_configuration_variable("file_type", allowed_types=[int, list, dict, None, float])
-            self.assertEqual(reply_json, "json")
+            parser.read_configuration_variable("file_type", allowed_types=[int, list, dict, None, float])
 
     def test_parser_read_configuration_variable_check_allowed_in_types_list(self):
         parser = ParseIt(config_folder_location=test_files_location)
